4cfc36c4f3dd5b80180b8eda4d6a40380fad92b8
ceivg <ceivg@LAPTOP-O0TP70JS>
2020-02-23 14:31:33 -0600

    v1

---
+++ src/ejericicio/pkg7/Ejericicio7.java
@@ -0,0 +1,16 @@
+package ejericicio.pkg7;
+
+public class Ejericicio7 {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(37));
+        System.out.println(Math.sin(300));
+        System.out.println(Math.cos(300));
+        System.out.println(Math.floor(22.8));
+        System.out.println(Math.ceil(22.8));
+        System.out.println(Math.round(22.8));
+        System.out.println(Math.max('D',(int)71));
+        System.out.println(Math.min('D',(int)71));
+        System.out.println(Math.round(Math.random()*20));        
+    }
+    
+}
